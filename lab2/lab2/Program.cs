4bb7922cbb1ad9fa63aabac36e2295f5557b9e6d
donFranko99 <franciszek.rybski@microsoft.wsei.edu.pl>
2021-03-18 14:12:42 +0100

    another program

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -6,11 +6,28 @@ namespace lab2
     {
         static void Main(string[] args)
         {
+            /*
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
             //int  a = int.Parse(napis);
-            int suma = a + b;
+            int suma = a + b +c;
             Console.WriteLine(suma);
+            */
+            int suma = 0;
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == null || line == string.Empty)
+                {
+                    break;
+                } else
+                {
+                    suma += int.Parse(line);
+                }
+                Console.WriteLine(suma);
+            }
         }
     }
 }
