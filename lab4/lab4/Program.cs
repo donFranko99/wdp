4af546193823933f8889ccea0f7e0400d0f1e932
donFranko99 <79981376+donFranko99@users.noreply.github.com>
2021-04-01 14:59:43 +0200

    lab4ex1

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstLength = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int[] firstTable = Array.ConvertAll<string, int>(input.Split(" "), int.Parse);
+            int secondLength = int.Parse(Console.ReadLine());
+            input = Console.ReadLine();
+            int[] secondTable = Array.ConvertAll<string, int>(input.Split(" "), int.Parse);
+
+            int counter = 0;
+            int[] controlTable = new int[firstLength];
+
+            for (int i = 0; i < firstLength; i++)
+            {
+                bool found = false;
+                for (int j = 0; j < secondLength; j++)
+                {
+                    if (firstTable[i] == secondTable[j])
+                    {
+                        found = true;
+                        continue;
+                    }
+                }
+                if (!found)
+                {
+                    controlTable[counter] = firstTable[i];
+                    counter++;
+                }
+            }
+            for (int i = 0; i < counter; i++)
+            {
+                Console.Write($"{controlTable[i]} ");
+            }
+        }
+    }
+}
